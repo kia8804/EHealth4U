aae28819a3541c2075e5ac9ec32e015e94ab926c
kia8804 <mirkamandarikiarash@gmail.com>
2022-05-18 21:11:56 -0400

    Initial Commit

---
+++ src/hospitalmanager/Startup.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospitalmanager;
+
+/**
+ *
+ * @author guest123
+ */
+public class Startup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
